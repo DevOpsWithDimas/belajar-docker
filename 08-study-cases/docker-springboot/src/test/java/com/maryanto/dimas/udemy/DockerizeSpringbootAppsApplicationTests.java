25ed005eb50d1a930c60db8b98d74229f19c4fbf
dimasm93 <software.dimas_m@icloud.com>
2021-07-04 01:32:43 +0700

    init project spring-boot

---
+++ 08-study-cases/docker-springboot/src/test/java/com/maryanto/dimas/udemy/DockerizeSpringbootAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizeSpringbootAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
